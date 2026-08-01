59d77bbb2f42d158073c6d24e8a865b75aa99cb7
Jose Jimenez <jjsalinas@pm.me>
2026-08-01 12:31:31 +0200

    doom-emacs add cpp lsp

--- doom-emacs/init.el
+++ doom-emacs/init.el
@@ -101,7 +101,7 @@
    :lang
    ;;ada                ; In strong typing we (blindly) trust
    ;;(agda +local)      ; types of types of types of types...
-   ;;(cc +lsp)          ; C > C++ == 1
+   (cc +lsp)          ; C > C++ == 1
    ;;clojure             ; java with a lisp
    ;;common-lisp         ; if you've seen one lisp, you've seen them all
    ;;coq                 ; proofs-as-programs
